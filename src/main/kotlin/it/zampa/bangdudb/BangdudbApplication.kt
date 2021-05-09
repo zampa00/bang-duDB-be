53786e30cbb84cbf2ca16fbd509b55d3ff599741
zampa <zampa92@gmail.com>
2021-05-09 15:10:09 +0200

    first commit with spring autogenerated stuff

---
+++ src/main/kotlin/it/zampa/bangdudb/BangdudbApplication.kt
@@ -0,0 +1,11 @@
+package it.zampa.bangdudb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BangdudbApplication
+
+fun main(args: Array<String>) {
+	runApplication<BangdudbApplication>(*args)
+}
