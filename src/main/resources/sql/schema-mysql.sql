f6d443ad64adee9acf75463f2537bb6d09e244b2
zampa <zampa92@gmail.com>
2021-05-25 15:43:00 +0200

    try changing something in db

--- src/main/resources/sql/schema-mysql.sql
+++ src/main/resources/sql/schema-mysql.sql
@@ -12,7 +12,7 @@ create table banners
 create table cards
 (
     id                          int(5) auto_increment primary key,
-    banner_id                   varchar(4)   not null,
+    banner_id                   varchar(4),
     card_id                     varchar(8)   not null,
     character_name              varchar(128) not null,
     band                        varchar(32)  not null,
@@ -41,5 +41,8 @@ create table cards
     src_base_lq                 varchar(255) not null,
     src_idl_lq                  varchar(255) not null,
     src_base_hq                 varchar(255) not null,
-    src_idl_hq                  varchar(255) not null
+    src_idl_hq                  varchar(255) not null,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (banner_id) REFERENCES banners (id)
 );
